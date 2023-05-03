84bc63318a3871466a758fc81b36030de9fde81d
Aizada <aizadaabdyrazakova3@gmail.com>
2023-05-03 19:42:31 +0600

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,39 @@
+public class Main {
+    public static void main(String[] args) {
+
+      Rectangle rectangle=new Rectangle();
+
+      rectangle.setWidth(7);
+      rectangle.setHeight(4);
+
+        System.out.println(rectangle.getWidth());
+        System.out.println(rectangle.getHeight());
+        System.out.println("Ares: "+rectangle.width*rectangle.height);
+
+
+
+
+
+
+
+//        Person person = new Person();
+//
+//        person.setName("Aiza");
+//        person.setAge(18);
+//        person.setEmail("esdrcftvgybh.com");
+//
+//        System.out.println(person.getName());
+//        System.out.println(person.getAge());
+//        System.out.println(person.getEmail());
+
+//        person.name="Daniel";
+//        person.age=18;
+//        person.email="danxedcfgvbh.com";
+//
+//        System.out.println(person.name);
+//        System.out.println(person.age);
+//        System.out.println(person.email);
+
+
+    }
+}
---
+++ src/Rectangle.java
@@ -0,0 +1,15 @@
+public class Rectangle {
+
+    int width;
+    int height;
+
+    public void setWidth(int width) {
+       this.width=width;
+    }public int getWidth(){
+        return width;
+    } public void setHeight(int height){
+        this.height=height;
+    }public int getHeight(){
+        return height;
+    }
+}
